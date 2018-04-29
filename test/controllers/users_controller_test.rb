64e44853e9e67a536f00415e5a80a1ed6c21cb05
paul <pcalnan71@gmail.com>
2018-04-29 17:00:33 +1000

    start implementation of follower relationship mvc

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -59,7 +59,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_no_difference 'User.count' do
       delete user_path(@user)
     end
-    assert_redirected_to root_url
+    assert_redirected_to login_url
   end
 
   test "should redirect destroy when logged in as a non-admin" do
@@ -68,5 +68,15 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
       delete user_path(@user)
     end
     assert_redirected_to root_url
+  end
+
+  test "should redirect following when not logged in" do
+    get following_user_path(@user)
+    assert_redirected_to login_url
+  end
+
+  test "should redirect followers when not logged in" do
+    get followers_user_path(@user)
+    assert_redirected_to login_url
   end
 end
